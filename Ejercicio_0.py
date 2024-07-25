c3feb700e1fe744ff37e99730d192a37dc9c5c1a
sebastian <132629506+Sebxzttian@users.noreply.github.com>
2024-07-24 22:06:47 -0500

    ejercicio0

---
+++ Ejercicio_0.py
@@ -0,0 +1,102 @@
+''' tres = three = drei = 3
+
+num1=type(4)
+print(num1)
+
+space_odysee=2001
+print(space_odysee)
+
+print(type('Good night & Good luck'))
+
+print(type(True))
+
+
+num1=10
+num2=3
+result=num1 + num2
+print(result)
+
+num1=10
+num2=3
+result=num1 - num2
+print(result)
+
+num1=10
+num2=3
+result=num1 * num2
+print(result)
+
+num1=10
+num2=3
+result=num1 / num2
+print(result)
+
+num1=10
+num2=3
+result=num1// num2
+print(result)
+
+num1=10
+num2=3
+result=num1 % num2
+print(result)
+
+num1=10
+num2=3
+result=num1 ** num2
+print(result)
+
+total_cars = 100
+sold_cars = 20
+total_cars = total_cars - sold_cars
+total_cars '''  
+
+
+#msg = 'Primera línea\nSegunda línea\nTercera línea'
+#print(msg)
+#Primera línea
+#Segunda línea
+#Tercera línea
+
+
+#msg = 'Valor = \t40'
+#print(msg)
+#Valor =     40
+
+
+#msg = 'Necesitamos \'escapar\' la comilla simple'
+#print(msg)
+#Necesitamos 'escapar' la comilla simple
+
+'''print(msg)
+Capítulo \ Sección \ Encabezado
+
+msg1 = '¿Sabes por qué estoy acá?'
+msg2 = 'Porque me apasiona'
+
+print(msg1, msg2)
+
+print(msg1, msg2, sep='|')
+
+print(msg2, end='!!')
+
+num1 = input ('ingrese un numero ')
+num2 = imput ('ingrese otro numero ')
+
+suma = int(num1) + int(num2)
+resta = num1 - num2
+multiplicacion = num1 * num2 
+division = num1 / num2
+
+print("la suma de los numeros es: " + (suma) 
+
+
+reaction = 'Wow'
+
+reaction * 4 '''
+
+name = 'Elon Musk'
+age = 49
+fortune = 43_300
+
+f'Me llamo {name}, tengo {age} años y una fortuna de {fortune} millones'
