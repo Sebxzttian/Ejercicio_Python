9050f3d70615c4ed0cc36afc26d42c379b711c5e
sebastian <132629506+Sebxzttian@users.noreply.github.com>
2024-07-31 22:12:58 -0500

    bucles y listas

---
+++ Ejercicio_Bucle.py
@@ -0,0 +1,36 @@
+'''want_greet = 'S'  # importante dar un valor inicial
+
+while want_greet == 'S':
+    print('Hola qué tal!')
+    want_greet = input('¿Quiere otro saludo? [S/N] ')
+print('Que tenga un buen día')'''
+
+'''MAX_GREETS = 4
+
+num_greets = 0
+want_greet = 'S'
+
+while want_greet == 'S':
+    print('Hola qué tal!')
+    num_greets += 1
+    if num_greets == MAX_GREETS:
+        print('Máximo número de saludos alcanzado')
+        break
+    want_greet = input('¿Quiere otro saludo? [S/N] ')
+print('Que tenga un buen día')'''
+
+'''MAX_GREETS = 4
+
+num_greets = 0
+want_greet = 'S'
+
+while want_greet == 'S':
+    print('Hola qué tal!')
+    num_greets += 1
+    if num_greets == MAX_GREETS:
+        print('Máximo número de saludos alcanzado')
+        break
+    want_greet = input('¿Quiere otro saludo? [S/N] ')
+else:
+    print('Usted no quiere más saludos')
+print('Que tenga un buen día')'''
---
+++ Ejercicio_Lsitas.py
@@ -0,0 +1,19 @@
+'''empty_list = []
+
+languages = ['Python', 'Ruby', 'Javascript']
+
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13]
+
+data = ['Tenerife', {'cielo': 'limpio', 'temp': 24}, 3718, (28.2933947, -16.5226597)]'''
+
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping[0]
+
+shopping[1]
+
+shopping[2]
+
+shopping[-1]  # acceso con índice negativo'''
+
+
